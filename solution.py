7f2f17898260c6746e08433d7bfe13a622c64053
tatianacue <122386464+tatianacue@users.noreply.github.com>
2024-04-10 16:23:15 -0400

    Create solution.py
    Creates window first

---
+++ solution.py
@@ -0,0 +1,21 @@
+import pygame
+pygame.init()
+
+WIDTH, HEIGHT = 700, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong")
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
